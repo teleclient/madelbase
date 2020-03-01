93a6de68a4a16fca682997dc38d87db246c27d93
esfand <esfand@wow.com>
2020-03-01 06:10:32 -0800

    Cleanup login.php.

--- login.php
+++ login.php
@@ -16,34 +16,11 @@ $MadelineProto = new \danog\MadelineProto\API('login.madeline', $settings);
 $MadelineProto->async(true);
 
 $MadelineProto->loop(function () use ($MadelineProto, $settings) {
-    //yield $MadelineProto->start();
-    //yield $MadelineProto->setEventHandler('\EventHandler');
-
     $wrapper = new \danog\MadelineProto\MyTelegramOrgWrapper($settings);
     $wrapper->async(true);
-
     yield $wrapper->login($GLOBALS['ACCOUNT_PHONE']);
-
     yield $wrapper->completeLogin(yield $wrapper->readline('Enter the code'));
-
     if (yield $wrapper->loggedIn()) {
-        if (yield $wrapper->hasApp()) {
-            $app = yield $wrapper->getApp();
-        } else {
-            $app_title = yield $wrapper->readLine('Enter the app\'s name, can be anything: ');
-            $short_name = yield $wrapper->readLine('Enter the app\'s short name, can be anything: ');
-            $url = yield $wrapper->readLine('Enter the app/website\'s URL, or t.me/yourusername: ');
-            $description = yield $wrapper->readLine('Describe your app: ');
-
-            $app = yield $wrapper->createApp([
-                'app_title'     => $app_title,
-                'app_shortname' => $short_name,
-                'app_url'       => $url,
-                'app_platform'  => 'web',
-                'app_desc'      => $description
-            ]);
-        }
-
-        \danog\MadelineProto\Logger::log($app);
+        throw new Exception('Unsuccessful login!');
     }
 });
