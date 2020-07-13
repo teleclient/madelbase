c27be50577150e0f9d4e4110466c83cd01eeed28
teleclient <54826170+teleclient@users.noreply.github.com>
2020-07-13 02:44:55 -0700

    Add loop on/off/state

--- base.php
+++ base.php
@@ -6,6 +6,13 @@ if (!\file_exists('madeline.php')) {
 }
 require 'madeline.php';
 
+if(!file_exists('data')) {
+    mkdir('data');
+}
+if(!file_exists('./data/loopstate.json')) {
+    file_put_contents('./data/loopstate.json', 'off');
+}
+
 function getConfig(): array
 {
     if(file_exists('config.php')) {
@@ -39,12 +46,41 @@ function getSettings(): array
     }
 }
 
+function secondsToNexMinute(): int
+{
+    $now   = hrtime()[0]; // time();
+    $next  = intdiv($now + 60, 60) * 60;
+    $delay = $next - $now;
+    $delay = $delay > 60? 60 : $delay;
+    return $delay; // in sec
+}
+
 function toJSON($var, bool $pretty = true): string {
     $opts = JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES;
     $json = \json_encode($var, $opts | ($pretty? JSON_PRETTY_PRINT : 0));
     $json = ($json !== '')? $json : var_export($var, true);
     return $json;
 }
+
+
+function parseCommand(string $msg, string $prefixes = '!/', int $maxParams = 3): array
+{
+    $command = ['prefix' => '', 'verb' => '', 'params' => []];
+    $msg = trim($msg);
+    if($msg && strlen($msg) >= 2 && strpos($prefixes, $msg[0]) !== false) {
+        $verb = strtolower(substr(rtrim($msg), 1, strpos($msg.' ', ' ') - 1));
+        if(ctype_alnum($verb)) {
+            $command['prefix'] = $msg[0];
+            $command['verb']   = $verb;
+            $tokens = explode(' ', $msg, $maxParams + 1);
+            for($i = 1; $i < count($tokens); $i++) {
+                $command['params'][$i - 1] = trim($tokens[$i]);
+            }
+        }
+    }
+    return $command;
+}
+
 
 function safeStartAndLoop(int $maxRestarts, $MadelineProto, $genLoop = null): void {
     $restarts = 0;
@@ -85,6 +121,7 @@ class EventHandler extends \danog\MadelineProto\EventHandler
     private $admins;
     private $startTime;
     private $reportPeers;
+    private $loopState;
 
     public function __construct(?\danog\MadelineProto\APIWrapper $API)
     {
@@ -93,6 +130,9 @@ class EventHandler extends \danog\MadelineProto\EventHandler
         $this->config      = getConfig();
         $this->admins      = [];
         $this->reportPeers = [];
+        //$this->loopState   = false;
+        $value = file_get_contents('data/loopstate.json');
+        $this->loopState = $value === 'on'? true: false;
     }
 
     public function getReportPeers()
@@ -104,6 +144,16 @@ class EventHandler extends \danog\MadelineProto\EventHandler
     {
         return $this->robotID;
     }
+
+    public function getLoopState(): bool
+    {
+        return $this->loopState;
+    }
+    public function setLoopState($loopState) {
+        $this->loopState = $loopState;
+        file_put_contents('data/loopstate.json', $loopState? 'on' : 'off');
+    }
+
 
     public function onStart(): \Generator
     {
@@ -148,8 +198,14 @@ class EventHandler extends \danog\MadelineProto\EventHandler
     }
     public function onUpdateNewMessage($update)
     {
-        if ($update['message']['_'] === 'messageEmpty') {
+        if ($update['message']['_'] === 'messageService' ||
+            $update['message']['_'] === 'messageEmpty')
+        {
             return;
+        }
+        if(!isset($update['message']['message'])) {
+            yield $this->echo(toJSON($update).PHP_EOL);
+            exit;
         }
         $msgOrig   = $update['message']['message']?? null;
         $msg       = $msgOrig? strtolower($msgOrig) : null;
@@ -162,39 +218,70 @@ class EventHandler extends \danog\MadelineProto\EventHandler
         $byRobot   = $fromId    === $this->robotID && $msg;
         $toRobot   = $replyToId === $this->robotID && $msg;
 
+        $command = parseCommand($msgOrig);
+        $verb    = $command['verb']?? null;
+        $params  = $command['params'];
+        if($verb) {
+            yield $this->echo(toJSON($command,  false).PHP_EOL);
+        }
+
         //  log the messages of the robot, or a reply to a message sent by the robot.
         if($byRobot || $toRobot) {
-           yield $this->logger(toJSON($update), Logger::ERROR);
+           yield $this->logger(toJSON($update, false), Logger::ERROR);
         } else {
             //yield $this->logger(toJSON($update, false), Logger::ERROR);
         }
 
-        if($byRobot && strlen($msg) >= 6 && substr($msg, 0, 6) === 'robot ') {
-            $param = trim(substr($msg, 5));
-            switch($param) {
+        if($byRobot && $verb) {
+            switch($verb) {
                 case 'help':
                     yield $this->messages->editMessage([
                         'peer'       => $peer,
                         'id'         => $messageId,
                         'message'    =>
                             "<b>Robot Instructions:</b><br>".
-                            "<br>".
-                            ">> <b>robot help</b><br>".
+                          //"<br>".
+                            ">> <b>/help</b><br>".
                             "   To print the robot commands' help.<br>".
-                            ">> <b>robot status</b><br>".
+                            ">> <b>/loop</b> on/off/state<br>".
+                            "   To query/change state of task repeater.<br>".
+                            ">> <b>/status</b><br>".
                             "   To query the status of the robot.<br>".
-                            ">> <b>robot uptime</b><br>".
+                            ">> <b>/uptime</b><br>".
                             "   To query the robot's uptime.<br>" .
-                            ">> <b>robot memory</b><br>".
+                            ">> <b>/memory</b><br>".
                             "   To query the robot's memory usage.<br>" .
-                            ">> <b>robot restart</b><br>".
+                            ">> <b>/restart</b><br>".
                             "   To restart the robot.<br>".
-                            ">> <b>robot stop</b><br>".
+                            ">> <b>/stop</b><br>".
                             "   To stop the script.<br>".
-                            ">> <b>robot logout</b><br>".
-                            "   To terminate the robot's session.<br>",
+                            ">> <b>/logout</b><br>".
+                            "   To terminate the robot's session.<br>".
+                            "<br>".
+                            "<b>**Valid prefixes are / and !</b><br>",
                         'parse_mode' => 'HTML',
                     ]);
+                    break;
+                case 'loop':
+                    $param = strtolower($params[0]??'');
+                    if(($param === 'on' || $param === 'off' || $param === 'state') && count($params) === 1) {
+                        $loopStatePrev = $this->getLoopState();
+                        $loopState = $param === 'on'? true : ($param === 'off'? false : $loopStatePrev);
+                        yield $this->messages->editMessage([
+                            'peer'    => $peer,
+                            'id'      => $messageId,
+                            'message' => 'The loop is ' . ($loopState? 'ON' : 'OFF') . '!',
+                        ]);
+                        if($loopState !== $loopStatePrev) {
+                            $this->setLoopState($loopState);
+                        }
+                    } else {
+                        yield $this->messages->editMessage([
+                            'peer'    => $peer,
+                            'id'      => $messageId,
+                            'message' => "The argument must be 'on', 'off, or 'state'.",
+                        ]);
+                    }
                     break;
                 case 'status':
                     yield $this->messages->editMessage([
@@ -283,15 +370,20 @@ $MadelineProto->async(true);
 $genLoop = new GenericLoop(
     $MadelineProto,
     function () use($MadelineProto) {
-        $msg = 'Time is '.date('H:i:s').'!';
-        yield $MadelineProto->logger($msg, Logger::ERROR);
         $eh = $MadelineProto->getEventHandler();
-        $robotID = $eh->getRobotID();
-        //yield $MadelineProto->messages->sendMessage([
-        //    'peer'    => $robotID,
-        //    'message' => $msg
-        //]);
-        return 120; // Repeat 60 seconds later
+        if($eh->getLoopState()) {
+            yield $MadelineProto->account->updateProfile([
+                'about' => date('H:i:s')
+            ]);
+            //$robotID = $eh->getRobotID();
+            //yield $MadelineProto->messages->sendMessage([
+            //    'peer'    => $robotID,
+            //    'message' => date('H:i:s')
+            //]);
+            //yield $MadelineProto->logger($msg, Logger::ERROR);
+        }
+        $delay = yield secondsToNexMinute($MadelineProto);
+        return $delay; // Repeat around 60 seconds later
     },
     'Repeating Loop'
 );
